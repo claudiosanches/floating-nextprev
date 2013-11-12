24d96f66836f8817d58be244c4784cb0b39896e8
claudiosmweb <contato@claudiosmweb.com>
2013-11-12 00:33:08 -0200

    added initial plugin file

---
+++ floating-nextprev.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Floating NextPrev.
+ *
+ * @package   Floating_NextPrev
+ * @author    Claudio Sanches <contato@claudiosmweb.com>
+ * @license   GPL-2.0+
+ * @link      https://github.com/claudiosmweb/floating-nextprev
+ * @copyright 2013 Claudio Sanches
+ *
+ * @wordpress-plugin
+ * Plugin Name: Floating NextPrev
+ * Plugin URI: https://github.com/claudiosmweb/floating-nextprev
+ * Description: TODO.
+ * Version: 2.0.0
+ * Author: claudiosanches
+ * Author URI: http://claudiosmweb.com/
+ * Text Domain: floating-nextprev
+ * License: GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path: /languages
+ * GitHub Plugin URI: https://github.com/claudiosmweb/floating-nextprev
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) die;
