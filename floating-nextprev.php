70530e57d2999a221a2b1deb5140f842d5cb0cb4
claudiosmweb <contato@claudiosmweb.com>
2013-11-13 02:46:33 -0200

    include classes

--- floating-nextprev.php
+++ floating-nextprev.php
@@ -24,3 +24,23 @@
 
 // If this file is called directly, abort.
 if ( ! defined( 'WPINC' ) ) die;
+
+require_once plugin_dir_path( __FILE__ ) . '/includes/class-floating-nextprev.php';
+
+/**
+ * Install default settings.
+ */
+register_activation_hook( __FILE__, array( 'Floating_NextPrev', 'install' ) );
+
+/**
+ * Load plugin instance.
+ */
+add_action( 'plugins_loaded', array( 'Floating_NextPrev', 'get_instance' ) );
+
+/**
+ * Plugin admin.
+ */
+if ( is_admin() && ( ! defined( 'DOING_AJAX' ) || ! DOING_AJAX ) ) {
+    require_once plugin_dir_path( __FILE__ ) . '/includes/class-floating-nextprev-admin.php';
+    add_action( 'plugins_loaded', array( 'Floating_NextPrev_Admin', 'get_instance' ) );
+}
