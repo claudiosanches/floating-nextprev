d67ffec7b98998354434974b33287c7ea80b26f1
claudiosmweb <contato@claudiosmweb.com>
2013-11-13 19:40:13 -0200

    added uninstall.php

---
+++ uninstall.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Uninstall actions.
+ *
+ * @package   Floating_NextPrev
+ * @author    Claudio Sanches <contato@claudiosmweb.com>
+ * @license   GPL-2.0+
+ * @link      https://github.com/claudiosmweb/floating-nextprev
+ * @copyright 2013 Claudio Sanches
+ */
+
+// If uninstall not called from WordPress, then exit.
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) )
+    exit;
+
+// Remove plugin options.
+delete_option( 'floating_nextprev' );
